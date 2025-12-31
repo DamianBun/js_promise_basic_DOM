28010a0b19a7156ad04f54d2704e8c346f52390e
damian Buń <damian.bun00@gmail.com>
2025-12-31 12:04:16 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,29 @@
 'use strict';
 
-new Promise((resolve, reject) => {
+// eslint-disable-next-line no-unused-vars
+const promise1 = new Promise((resolve, reject) => {
   const logo = document.getElementsByClassName('logo')[0];
 
   logo.addEventListener('click', () => {
     resolve();
   });
-}).then(() => {
-  document.body.innerHTML += `<div class="message">Promise was resolved!</div>`;
-});
+})
+  .then(() => {
+    document.body.innerHTML += `<div class="message">Promise was resolved!</div>`;
+  })
+  .catch(() => {
+    document.body.innerHTML += `<div class="message error-message">Promise was rejected!</div>`;
+  });
 
-new Promise((resolve, reject) => {
+// eslint-disable-next-line no-unused-vars
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(new Error('rejected'));
   }, 3000);
-}).catch(() => {
-  document.body.innerHTML +=
-    '<div class="message error-message">Promise was rejected!</div>';
-});
+})
+  .then(() => {
+    document.body.innerHTML += `<div class="message">Promise was resolved!</div>`;
+  })
+  .catch(() => {
+    document.body.innerHTML += `<div class="message error-message">Promise was rejected!</div>`;
+  });
