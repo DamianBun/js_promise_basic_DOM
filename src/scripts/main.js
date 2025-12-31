9644217d4ee144ae87ec32268bb777761295da4f
damian Buń <damian.bun00@gmail.com>
2025-12-31 11:17:52 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+new Promise((resolve, reject) => {
+  const logo = document.getElementsByClassName('logo')[0];
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+}).then(() => {
+  document.body.innerHTML += `<div class="message">Promise was resolved!</div>`;
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('rejected'));
+  }, 3000);
+}).catch(() => {
+  document.body.innerHTML +=
+    '<div class="message error-message">Promise was rejected!</div>';
+});
